8d50aab65c82fd6cd767756e3ee02107c97597e4
Chetan Singh <chetan.singh@webeesocial.com>
2022-10-04 22:33:41 +0530

    inital commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends \TCG\Voyager\Http\Controllers\VoyagerBaseController
+{
+
+    
+
+}
